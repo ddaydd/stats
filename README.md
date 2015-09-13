ee80f16a3958802276c9a782e5d8e0635c21cbd6
David FAIVRE-MAÇON <faivred@gmail.com>
2015-09-13 22:46:53 +0400

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # dayd:stats ( WIP )
 
+Github:
+https://github.com/ddaydd/stats
+
+AtmosphereJS:
+https://atmospherejs.com/dayd/stats
+
+Report bugs or suggestions:
+https://github.com/ddaydd/stats/issues
+
 ## Installation
 
 ```sh
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'dayd:stats',
-  version: '0.0.82',
+  version: '0.0.84',
   // Brief, one-line summary of the package.
   summary: "Visitor stat package to log visits on your site automatically",
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ddaydd/stats.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
