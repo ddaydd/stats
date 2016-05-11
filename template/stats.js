7c985634a373535b7f00b45296ecd413ba932037
judyrandry <judyrandry@gmail.com>
2016-05-11 12:24:54 +0300

    create statsByPath helpers

--- template/stats.js
+++ template/stats.js
@@ -42,10 +42,15 @@ Template.daydStats.helpers({
       if(u && u.username)
         return u.username;
     }
-
-
+  },
+  statsByPath:function(){
+    Meteor.call("statsNotFilteredGroupedPathCount", this.customPath, function(err, result) {
+      if(err)
+        return console.log(err);
+      Session.set('stats_list_by_path', result);
+    });
+    return Session.get('stats_list_by_path');
   }
-
 });
 
 
