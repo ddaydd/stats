30c4f711fe81043d19b477bb354c72262647dfdf
judyrandry <judyrandry@gmail.com>
2016-05-12 12:39:16 +0300

    FIX exception in publish

--- server/publish.js
+++ server/publish.js
@@ -9,6 +9,8 @@ Meteor.publish("daydStatsPath", function() {
 Meteor.publish("daydStatsReferer", function() {
   return DaydStatsReferer.find();
 });
-Meteor.publish("daydStatsUserById", function(id){
-  return DaydStatsUsers.findOne({_id: id});
+Meteor.publish("daydStatsUserById", function(id) {
+  if(id)
+    return DaydStatsUsers.find({_id: id});
+  return DaydStatsUsers.find();
 });
