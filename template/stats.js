b7da1a8f9f1ce1124d5b5c973fd2bd82d8c1a690
judyrandry <judyrandry@gmail.com>
2016-05-23 11:03:39 +0300

    send date parameter in statsByPath

--- template/stats.js
+++ template/stats.js
@@ -83,7 +83,7 @@ Template.daydStats.events({
 });
 Template.daydStatsPath.helpers({
   statsByPath: function() {
-    Meteor.call("statsNotFilteredGroupedPathCount", this.customPath, this.userSelected, this.all, this.hide, function(err, result) {
+    Meteor.call("statsNotFilteredGroupedPathCount", this.customPath, this.userSelected, this.all, this.hide, this.date, function(err, result) {
       if(err)
         return console.log(err);
       Session.set('stats_list_by_path', result);
