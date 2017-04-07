e6d53fad2f033ca2ab139cc674f32ea35681eb01
Mickael Faivre-Maçon <mfm@enginee-ring.com>
2017-04-07 19:24:42 +0200

    log => error

--- template/stats.js
+++ template/stats.js
@@ -38,7 +38,7 @@ Template.daydStats.helpers({
 
   username: function() {
     if(this.userId) {
-      var u = Meteor.users.findOne(this.userId);
+      const u = Meteor.users.findOne(this.userId);
       if(u && u.username)
         return u.username;
     }
@@ -85,7 +85,7 @@ Template.daydStatsPath.helpers({
   statsByPath: function() {
     Meteor.call("statsNotFilteredGroupedPathCount", this.customPath, this.userSelected, this.all, this.hide, this.date, function(err, result) {
       if(err)
-        return console.log(err);
+        return console.error(err);
       Session.set('stats_list_by_path', result);
     });
     return Session.get('stats_list_by_path');
@@ -93,7 +93,7 @@ Template.daydStatsPath.helpers({
   statsNumberOfUsersDistinct: function() {
     Meteor.call("statsUsersDistinct", this.distinct, this.hide, this.date, function(err, result) {
       if(err)
-        return console.log(err);
+        return console.error(err);
       Session.set('stats_number_of_users_distinct', result);
     });
     return Session.get('stats_number_of_users_distinct');
@@ -101,7 +101,7 @@ Template.daydStatsPath.helpers({
   usersNumbersCount: function() {
     Meteor.call('statsUsersCount', this.hide, this.date, function(err, result) {
       if(err)
-        return console.log(err);
+        return console.error(err);
       Session.set('stats_users_count', result);
     });
     return Session.get('stats_users_count');
@@ -110,7 +110,7 @@ Template.daydStatsPath.helpers({
   usersWithConnectionDuration: function() {
     Meteor.call('getStatsDurationConnectionAverage', this.userSelected, this.all, this.hide, this.date, function(err, result) {
       if(err)
-        console.log(err);
+        console.error(err);
       Session.set('stats_user_with_duration', result);
     });
     return Session.get('stats_user_with_duration');
@@ -121,14 +121,14 @@ Template.daydStatsPath.helpers({
   pathPerConnection: function() {
     Meteor.call('getPathsPerUserConnection', this.userSelected, this.all, this.hide, this.date, function(err, result) {
       if(err)
-        console.log(err);
+        console.error(err);
       Session.set('stats_pages_per_connection', result);
     });
     return Session.get('stats_pages_per_connection');
   },
   durationEnterPaths: function() {
     Meteor.call('getDurationConnectionPaths', this.customPaths, this.userSelected, this.all, this.hide, this.date, function(err, result) {
-      if(err)console.log(err);
+      if(err)console.error(err);
       Session.set('stats_duration_enter_paths', result);
     });
     return Session.get('stats_duration_enter_paths');
@@ -137,43 +137,43 @@ Template.daydStatsPath.helpers({
     return formatDuration(this.avgConnectionPaths);
   },
   customStatsList: function() {
-    var c = this.customName;
+    const c = this.customName;
     Meteor.call('getCustomStatsWithParameter', c, this.userSelected, this.all, this.hide, this.date, function(err, resust) {
-      if(err)console.log(err);
+      if(err)console.error(err);
       Session.set('stats_custom_listing_' + c, resust);
     });
     return Session.get('stats_custom_listing_' + c);
   },
   customCountNumbers: function() {
-    var c = this.customName;
+    const c = this.customName;
     Meteor.call('getCustomStatsCountWithParameter', c, this.userSelected, this.all, this.hide, this.date, function(err, result) {
-      if(err) console.log(err);
+      if(err) console.error(err);
       Session.set('stats_count_per_custom_name_' + c, result);
     });
     return Session.get('stats_count_per_custom_name_' + c);
   },
   usersConnectedInRealTime: function() {
     Meteor.call('getStatsUsersConnectedInRealTime', this.hide, function(err, res) {
-      if(err)console.log(err);
+      if(err)console.error(err);
       Session.set('stats_users_connected_in_real_time', res);
     });
     return Session.get('stats_users_connected_in_real_time');
   },
   numberOfUsersConnected: function() {
-    var u = Session.get('stats_users_connected_in_real_time');
+    const u = Session.get('stats_users_connected_in_real_time');
     if(u)
       return u.length;
   },
   lastDateConnection: function() {
     Meteor.call('getStatsUsersLastConnection', this.userSelected, this.date, function(err, res) {
-      if(err) console.log(err);
+      if(err) console.error(err);
       return Session.set('stats_user_last_connection', res);
     });
     return Session.get('stats_user_last_connection');
   },
   numberVisitsPerUser: function() {
     Meteor.call('getNumberStatsVisitsPerUser', this.userSelected, this.date, function(err, res) {
-      if(err) console.log(err);
+      if(err) console.error(err);
       return Session.set('stats_visits_number_per_user', res);
     });
     return Session.get('stats_visits_number_per_user');
@@ -187,7 +187,7 @@ Template.daydStatsPath.helpers({
 });
 
 function formatDuration(ms) {
-  var duration = moment.duration(ms);
+  const duration = moment.duration(ms);
   if(duration.asHours() > 1) {
     return Math.floor(duration.asHours()) + moment.utc(duration.asMilliseconds()).format(":mm:ss");
   } else {
